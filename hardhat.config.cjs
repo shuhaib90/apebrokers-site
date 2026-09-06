cc754bfe384d8cfc1abacba88697eea1e2e866a3
shuhaib90 <shuhaib90@users.noreply.github.com>
2026-09-06 10:20:36 +0530

    chore: default robinhood network chainId to 4663 in hardhat config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
     },
     robinhood: {
       url: process.env.RPC_URL || "https://robinhood-mainnet.g.alchemy.com/v2/alch_008u8jC_qTSIJvqgLbdGY",
-      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 1337,
+      chainId: process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID) : 4663,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
